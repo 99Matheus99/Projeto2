e7fda64dde6276c02ca553edd2c499a7bf2da645
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-08 22:49:49 -0300

    Implementação de mais um método que calcula o imc

--- Classes.py
+++ Classes.py
@@ -7,12 +7,17 @@ class Pessoa:
         Self.peso = peso
     
     #métodos da minha classe(todos os métodos devem possuir Self)
-    def eh_maior(Self):
+    def eh_maior(Self): 
         if Pessoa1.idade >= 18:
             return True
         else:
             return False
-
+    def imc(Self):
+        imc = Pessoa1.peso / (Pessoa1.altura * Pessoa1.altura)
+        return imc
+    
+#teste da função(será apagado)
 Pessoa1 = Pessoa("Matheus", 21, 1.60, 60)
 
-Pessoa1.eh_maior()
+print(Pessoa1.eh_maior())
+print(Pessoa1.imc())
