79ed0f68c5b522197cc868d5a61b3268df5d7515
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-08 22:15:24 -0300

    Criação da classe e seus parâmetros iniciais

---
+++ Classes.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    def __init__(Self, nome, idade, altura, peso): # função construtora da minha classe
+        #aqui vão ficar os parâmetros da minha classe
+        Self.nome = nome
+        Self.idade = idade
+        Self.altura = altura
+        Self.peso = peso
+        
