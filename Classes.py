3489e4f54734f537afb217665f0962709b0dc1d0
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-08 23:17:02 -0300

    Implementação do método imc_longo

--- Classes.py
+++ Classes.py
@@ -15,9 +15,23 @@ class Pessoa:
     def imc(Self):
         imc = Pessoa1.peso / (Pessoa1.altura * Pessoa1.altura)
         return imc
-    
+    def imc_longo(Self):
+        imc = Pessoa1.peso / (Pessoa1.altura * Pessoa1.altura)
+        if 18 <= imc < 25:
+            return "Peso normal"
+        elif 25 <= imc < 30:
+            return "Pré-obesidade"
+        elif 30 <= imc < 35:
+            return "Obesidade Grau 1"
+        elif 35 <= imc < 40:
+            return "Obesidade Grau 2"
+        else:
+            return "obesidade Grau 3"
+        
+
 #teste da função(será apagado)
 Pessoa1 = Pessoa("Matheus", 21, 1.60, 60)
 
 print(Pessoa1.eh_maior())
 print(Pessoa1.imc())
+print(Pessoa1.imc_longo())
