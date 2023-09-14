7c693b049e8042bd53a0474775943a3ff798a745
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-13 22:05:19 -0300

    primeira validação para o padrão que se seguirá

--- validações.py
+++ validações.py
@@ -1,7 +1,17 @@
 from classes import Pessoa
-nome = str(input('Digite seu nome: '))
-idade = int(input('Digite a idade: '))
-altura = float(input('Digite a altura: '))
-peso = float(input('Digite o peso: '))
-nascimento = str(input('Digite a data de nascimento: ')) # o str() é pra deixar bem claro que a data é dado em string
-Pessoa1 = Pessoa(nome, idade, altura, peso, nascimento)
+import re
+while True:
+    nome = str(input('Digite seu nome: '))
+    # no código de baixo, eu pego basicamente qualquer letra maiúscula, minúscula, com acentos, espaço em brando e com ponto no final
+    if re.match(r'^[a-zA-ZÀ-Úá-ú\s\.]+$', nome):
+        break
+    else:
+        print('ENTRADA INVÁLIDA! Digite apenas letras contidas em espaços')
+
+
+
+#idade = int(input('Digite a idade: '))
+#altura = float(input('Digite a altura: '))
+#peso = float(input('Digite o peso: '))
+#nascimento = str(input('Digite a data de nascimento: ')) # o str() é pra deixar bem claro que a data é dado em string
+#Pessoa1 = Pessoa(nome, idade, altura, peso, nascimento)
