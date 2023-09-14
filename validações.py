4590b8b738369989936a1c9ee02186d792654f5a
matheus de oliveira <nubinhopro5@gmail.com>
2023-09-13 22:51:09 -0300

    adição de validação da idade e do peso

--- validações.py
+++ validações.py
@@ -1,17 +1,29 @@
 from classes import Pessoa
 import re
 while True:
-    nome = str(input('Digite seu nome: '))
+    nome = input('Digite seu nome: ')
     # no código de baixo, eu pego basicamente qualquer letra maiúscula, minúscula, com acentos, espaço em brando e com ponto no final
     if re.match(r'^[a-zA-ZÀ-Úá-ú\s\.]+$', nome):
         break
     else:
         print('ENTRADA INVÁLIDA! Digite apenas letras contidas em espaços')
+while True:
+    idade = input('Digite a idade: ')
+    if re.match(r'^[0-9\s]+$', idade):
+        idade = int(idade) # no final da análise, transformo minha variável
+        break
+    else:
+        print('ENTRADA INVÁLIDA! Digite apenas números positivos')
+while True:
+    peso = input('Digite o peso: ')
+    # pego qualquer número inteiro antes do ponto, pego o ponto, e por fim qualquer número depois do ponto
+    if re.match(r'^[0-9]+\.[0-9]+$' , peso):
+        break
+    else:
+        print('ERRO! Digite apenas números flutuantes com ponto')
 
 
 
-#idade = int(input('Digite a idade: '))
-#altura = float(input('Digite a altura: '))
-#peso = float(input('Digite o peso: '))
+
 #nascimento = str(input('Digite a data de nascimento: ')) # o str() é pra deixar bem claro que a data é dado em string
 #Pessoa1 = Pessoa(nome, idade, altura, peso, nascimento)
